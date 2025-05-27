46755da08bd3dd74bf4d189bfcc4d729ff22202e
Lesedi Manganyi <2025054160@ufs.ac.za>
2025-05-27 19:40:50 +0200

    Little bit of updates to mainly Reception class

--- Receptionist.cs
+++ Receptionist.cs
@@ -8,18 +8,54 @@ namespace Salon_Appointment_System
 {
     public class Receptionist : Person
     {
+        private List<Appointment> appointments;
         public Receptionist(string firstName, string lastName, string username, string password, string staffId)
             : base(firstName, lastName, username, password, staffId)
         {
             //StaffId = staffId;
+            appointments = new List<Appointment>();
         }
         public void ScheduleAppointment(Appointment appointment)
         {
             // Logic to schedule an appointment
+            if (appointment != null)
+            {
+                appointments.Add(appointment);
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(appointment), "There is no appointment to schedule.");
+            }
         }
         public void CancelAppointment(Appointment appointment)
         {
             // Logic to cancel an appointment
+            if (appointment != null && appointments.Contains(appointment))
+            {
+                appointments.Remove(appointment);
+            }
+            else
+            {
+                throw new ArgumentException("Appointment not found or is null.");
+            }
         }
+
+        public void UpdateAppointment(Appointment appointment)
+        {
+            // Logic to update an appointment
+        }
+
+        public List<Appointment> ViewAppointments()
+        {
+            // Logic to view appointments
+            if (appointments.Count == 0)
+            {
+                throw new ArgumentException("There are no appointments");
+            }
+            return appointments;
+        }
+
+        //Add UpdateAppointment
+        //View Appointments
     }
 }
